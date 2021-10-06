61dfd706b53f7382ed40989acbc74c9e265e85a9
desertalex <alexbraddock22@gmail.com>
2021-10-05 18:44:57 -0600

    node js added

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-// first comment
-// second demo
+const express = require('express');
+const path = require('path');
+
+const PORT = 3000;
+
+const app = express();
+
+app.set('view engine', 'ejs')
+    .set('views', 'views')
+    .use(express.json())
+    // app.use(bodyparser.json()); //utilizes the body-parser package
+    .use(express.static(path.join(__dirname, 'public')))
+
+const routes = require('./routes/home')
+app.use('/', routes)
+
+app.listen(PORT)
